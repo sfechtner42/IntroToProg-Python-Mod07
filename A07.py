b94dff12537b7be998f9f28386c4c6fa29376fd7
sfechtner42 <sfechtner42@gmail.com>
2023-11-24 13:55:27 -0800

    Added Student Class Finsihed Debug, now adding __int__ Added Properties

--- A07.py
+++ A07.py
@@ -6,7 +6,6 @@
 #   Sabrina Fechtner, 11/24/2023, Imported A06
 # ------------------------------------------------------------------------------------------ #
 import json
-from typing import TextIO, List
 
 # Define the Data Constants
 MENU: str = '''
@@ -21,30 +20,8 @@ Select from the following menu:
 # Define the Data Constants
 FILE_NAME: str = "Enrollments.json"
 
-
-# Define Classes
-class Person:
-    pass
-
-
-class Student:
-    def __init__(self, student_first_name:str, student_last_name:str, course_name:str) -> None:
-        self._student_first_name = student_first_name
-        self._student_last_name = student_last_name
-        self._course_name = course_name
-    @property
-    def get_first_name(self) -> str:
-        return self._student_first_name.capitalize()
-    @property
-    def get_last_name(self) -> str:
-        return self._student_last_name.capitalize()
-    @property
-    def get_course_name(self) -> str:
-        return self._course_name.capitalize()
-
-
 # Define the Data Variables
-students: list[Student] = []
+students: list = []
 menu_choice: str
 
 
@@ -59,7 +36,7 @@ class FileProcessor:
     """
 
     @staticmethod
-    def read_data_from_file(file_name: str) -> list[Student]:
+    def read_data_from_file(file_name: str, student_data: list):
         """ This function reads previous JSON file with student and course data
 
         ChangeLog: (Who, When, What)
@@ -67,62 +44,47 @@ class FileProcessor:
         Sabrina Fechtner, 11.16.2023, Incorporated Function
 
         :param: file_name: string data with name of file to read from
+        :param: student_data: list of dictionary rows to be filled with file data
+
         :return: Student Data as list
         """
-        file: TextIO = None
-        json_data = []
-        students: list[Student] = []
         try:
-            file = open(file_name, "r")
-            json_data = json.load(file)
-            print("Data successfully loaded from the file.")
+            with open(file_name, "r") as file:
+                student_data = json.load(file)
+                print("Data successfully loaded from the file.")
         except FileNotFoundError:
             print("File not found, creating it...")
             with open(file_name, "w") as file:
-                json.dump(json_data, file)
+                json.dump(student_data, file)
                 print("File created successfully.")
         except json.JSONDecodeError as e:
             print(f"Invalid JSON file: {e}. Resetting it...")
             with open(file_name, "w") as file:
-                json.dump(json_data, file)
+                json.dump(student_data, file)
                 print("File reset successfully.")
         except Exception as e:
             print(f"An unexpected error occurred while loading data: {e}")
 
-        for row in json_data:
-            student = Student(row["student_first_name"], row["student_last_name"], row["course_name"])
-            students.append(student)
-
-        return students
+        return student_data
 
     @staticmethod
-    def write_data_to_file(roster: list[Student], file_name: str) -> list[Student]:
+    def write_data_to_file(file_name: str, student_data: list):
         """ This function writes student and course data to JSON file
 
         ChangeLog: (Who, When, What)
         RRoot,1.3.2030,Created function
         Sabrina Fechtner, 11.16.2023, Incorporated Function
-        :param: file name = JSON file and roster = student data
+
         :return: None
         """
-        file: TextIO = None
         try:
-            json_data: list[dict[str, str, str]] = []
-            for student in roster:
-                json_data.append({
-                    "student_first_name": student.get_first_name,
-                    "student_last_name": student.get_last_name,
-                    "course_name": student.get_course_name
-                }
-                )
             with open(file_name, "w") as file:
-                json.dump(json_data, file)
+                json.dump(student_data, file)
                 print("Data successfully written to the file.")
         except TypeError as e:
             IO.output_error_messages("Please check that the data is a valid JSON format", e)
         except Exception as e:
             IO.output_error_messages("There was a non-specific error!", e)
-        return roster
 
 
 # Present and Process the data
@@ -181,7 +143,7 @@ class IO:
         return choice
 
     @staticmethod
-    def output_student_courses(student_data: list[Student]):
+    def output_student_courses(student_data: list):
         """ This function shows the first name, last name, and course name from the user
 
         ChangeLog: (Who, When, What)
@@ -192,20 +154,18 @@ class IO:
         """
         print("\nThe current data is:")
         for student in student_data:
-            student_first_name = student.get_first_name
-            student_last_name = student.get_last_name
-            student_course_name = student.get_course_name
-            print(student_first_name,student_last_name, student_course_name)
+            print(student)
+
     @staticmethod
-    def input_student_data(student_data: list[Student]):
-        """ This function incorporates user choice from the menu
+    def input_student_data(student_data: list):
+        """ This function incorporates user choice from menu
 
-        ChangeLog: (Who, When, What)
-        RRoot,1.3.2030,Created function
-        Sabrina Fechtner, 11.16.2023, Incorporated into A06
+                ChangeLog: (Who, When, What)
+                RRoot,1.3.2030,Created function
+                Sabrina Fechtner, 11.16.2023, Incorporated into A06
 
-        :return: None
-        """
+                :return: None
+                    """
         while True:
             try:
                 student_first_name = input("Enter the student's first name: ")
@@ -224,18 +184,15 @@ class IO:
                 print(e)
 
         course_name = input("Please enter the name of the course: ")
-
-        student = Student(student_first_name, student_last_name, course_name)
-
+        student = {"student_first_name": student_first_name, "student_last_name": student_last_name,
+                   "course": course_name}
         student_data.append(student)
-
         print(f"You have registered {student_first_name} {student_last_name} for {course_name}.")
         return student_data
 
 
 # Main Program:
-
-students: list[Student] = FileProcessor.read_data_from_file(file_name=FILE_NAME)
+students = FileProcessor.read_data_from_file(file_name=FILE_NAME, student_data=students)
 
 while True:
     IO.output_menu(menu=MENU)
@@ -251,7 +208,7 @@ while True:
         continue
 
     elif menu_choice == "3":  # Save data in a file
-        FileProcessor.write_data_to_file(file_name=FILE_NAME, roster=students)
+        FileProcessor.write_data_to_file(file_name=FILE_NAME, student_data=students)
         continue
 
     elif menu_choice == "4":  # End the program
